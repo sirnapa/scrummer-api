51781b4c27552744c4d989a5451088287e788eeb
developer <jonatancubillos@gmail.com>
2016-04-08 22:23:47 -0400

    correciones en el branch desarrollo

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,7 @@ def prepare_deployment_with_tag(tag_name):
     local('python manage.py test scrummer')
     local('git add -p && git commit')
     local('git checkout master && git merge develop')
-    local('git tag -l "' + tag_name + '"')
+    local('git tag "' + tag_name + '"')
     local('git checkout develop')
 
 from fabric.api import lcd
