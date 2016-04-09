077c96b712d1bfb9ee1a104950dc38291b6f855b
developer <jonatancubillos@gmail.com>
2016-04-09 04:27:53 -0400

    agregamos archivos y modificaciones necesarias para uwsgi en produccion

--- scrummer_is/settings.py
+++ scrummer_is/settings.py
@@ -44,7 +44,13 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    'TEST_REQUEST_RENDERER_CLASSES': (
+        'rest_framework.renderers.MultiPartRenderer',
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.TemplateHTMLRenderer'
+    )
 }
 
 MIDDLEWARE_CLASSES = [
