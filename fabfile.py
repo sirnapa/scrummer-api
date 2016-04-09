9bb58911b55f12db8ccf997ab7f320822b1f8285
developer <jonatancubillos@gmail.com>
2016-04-08 21:58:37 -0400

    cambios menores al fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,9 +3,7 @@ from fabric.api import local
 def prepare_deployment(branch_name):
     local('python manage.py test scrummer')
     local('git add -p && git commit')
-    local('git push')
     local('git checkout master && git merge ' + branch_name)
-    local('git push')
     local('git checkout develop')
 
 from fabric.api import lcd
