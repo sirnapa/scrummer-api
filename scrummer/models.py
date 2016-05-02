ac41e9d2bc4f301fe05e13a728603208c3d77c73
developer <dbaezrenault@gmail.com>
2016-05-02 08:40:49 -0400

    Models.py

--- scrummer/models.py
+++ scrummer/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 
+
 class Proyecto(models.Model):
     nombre = models.CharField(max_length=30, verbose_name='Nombre')
     descripcion = models.CharField(max_length=50, verbose_name='Descripcion')
